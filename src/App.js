3ea3fb43f88e4c5c3d891d5f7766660b57158dc8
Seungil <seungilbang@khu.ac.kr>
2023-05-27 22:04:38 +0900

    [UPD] App.js: Routes & Login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Header from "./layout/Header";
+import { Route, Routes } from 'react-router-dom';
+import Login from "./api/Login";
+import GetUserInfo from "./api/GetUserInfo";
 
 function App() {
 
   return (
     <div className="App">
-      <Header/ >
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/token" element={<Login />} />
+        <Route path="/userinfo" element={<GetUserInfo />} />
+      </Routes>
     </div>
   );
 }
