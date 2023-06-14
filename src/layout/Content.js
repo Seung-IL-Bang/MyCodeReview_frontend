5e8a4b2803d2085eed463f88c5c07a0b529448ea
Seungil Linux <seungilbang@khu.ac.kr>
2023-06-14 16:00:45 +0900

    [UPD] Content.js: 게시글 리스트 생성

--- src/layout/Content.js
+++ src/layout/Content.js
@@ -1,6 +1,19 @@
-export default function Content() {
+import Board from "../component/Board";
+
+export default function Content(props) {
+
+
+  const boardList = props.list.map(board =>
+     <Board key={`board${board.id}`}
+            id={board.id} 
+            email={board.email} 
+            content={board.content} 
+            writer={board.writer} 
+            title={board.title} />)
   
   return (
-    <></>
+    <ul>
+      {boardList}
+    </ul>
   );
 }
