0e476f8e42d57cbe6a9a385f96baa51814379e0c
Seungil <seungilbang@khu.ac.kr>
2023-06-16 23:02:07 +0900

    [UPD] Board.js: 게시글 조회 페이지 이동 핸들러 추가

--- src/component/board/Board.js
+++ src/component/board/Board.js
@@ -1,16 +1,23 @@
 import Card from 'react-bootstrap/Card';
+import { useNavigate } from 'react-router';
+import classes from './Board.module.css'
 
 
 export default function Board(props) {
 
+  const navigate = useNavigate()
+
+  const moveToViewPage = () => {
+    navigate(`/review/${props.id}`)
+  }
 
   // props 로 받아와야 하는 변수
   // title, difficulty, Tags, Description, Link
 
   
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Body>
+    <Card className={classes.board_card} style={{ width: '18rem' }}>
+      <Card.Body onClick={moveToViewPage}>
         <Card.Title>{props.title}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">difficulty</Card.Subtitle>
         <Card.Text>
