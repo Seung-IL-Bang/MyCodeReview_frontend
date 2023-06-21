8fb0423c2ed01029055b7b8f58feaa9a3816c34e
Seungil <seungilbang@khu.ac.kr>
2023-06-21 23:44:26 +0900

    [UPD] Board.js: 난이도 추가

--- src/component/board/Board.js
+++ src/component/board/Board.js
@@ -1,36 +1,29 @@
-import Card from 'react-bootstrap/Card';
-import { useNavigate } from 'react-router';
-import classes from './Board.module.css'
-
+import Card from "react-bootstrap/Card";
+import { useNavigate } from "react-router";
+import classes from "./Board.module.css";
+import SelectedDifficulty from "../write/SelectedDifficulty";
 
 export default function Board(props) {
-
-  const navigate = useNavigate()
+  
+  const navigate = useNavigate();
 
   const tagList = props.tagList.map((tag, index) => (
     <div key={`tag${index}`}>{tag}</div>
   ));
 
+
   const moveToViewPage = () => {
-    navigate(`/review/${props.id}`)
-  }
+    navigate(`/review/${props.id}`);
+  };
 
-  // props 로 받아와야 하는 변수
-  // title, difficulty, Tags, Description, Link
-
-  
   return (
-    <Card className={classes.board_card} style={{ width: '18rem' }}>
+    <Card className={classes.board_card} style={{ width: "18rem" }}>
       <Card.Body onClick={moveToViewPage}>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">difficulty</Card.Subtitle>
-        <div className={classes.tag_list}>
-          {tagList}
+        <SelectedDifficulty difficulty={props.difficulty}/>
+        <div className={classes.tag_list_wrapper}>
+          <div className={classes.tag_list}>{tagList}</div>
         </div>
-        <Card.Text>
-          Description
-        </Card.Text>
-        <Card.Link href="#">Problem Link</Card.Link>
       </Card.Body>
     </Card>
   );
