3ec9e6a33bb3ac9b5d7c94283492ce4cd21d68db
Seungil <seungilbang@khu.ac.kr>
2023-06-21 23:44:58 +0900

    [UPD] MetaData.js: 링크와 난이도 추가

--- src/component/review/MetaData.js
+++ src/component/review/MetaData.js
@@ -3,7 +3,14 @@ import { Row, Col } from "react-bootstrap";
 import axios from "axios";
 import { useNavigate } from "react-router";
 import { useDispatch } from "react-redux";
-import { setTitle, setContent, setTagList } from "../../reducer/metaDataReducer";
+import {
+  setTitle,
+  setContent,
+  setTagList,
+  setLink,
+  setDifficulty
+} from "../../reducer/metaDataReducer";
+import SelectedDifficulty from "../write/SelectedDifficulty";
 
 export default function MetaData(props) {
   const navigate = useNavigate();
@@ -23,8 +30,15 @@ export default function MetaData(props) {
     dispatch(setTitle(props.data.title));
     dispatch(setContent(props.data.content));
     dispatch(setTagList(props.data.tagList));
+    dispatch(setLink(props.data.link));
+    dispatch(setDifficulty(props.data.difficulty))
     navigate(`/modify/${props.id}`);
   };
+
+  const handleGoToSource = (e) => {
+    e.preventDefault();
+    window.open(props.data.link, '_blank');
+  }
 
   const deleteById = async (id) => {
     const accessToken = localStorage.getItem("accessToken");
@@ -45,11 +59,19 @@ export default function MetaData(props) {
       <div className={classes.title}>{props.data.title}</div>
       <br />
       <Row className="justify-content">
+        <SelectedDifficulty difficulty={props.data.difficulty}/>
+      </Row>
+      <Row className="justify-content">
         <div className={classes.tag_list}>{tagList}</div>
       </Row>
       <Row className="justify-content-end">
         <Col>
           <div className={classes.action_links}>
+            <div className={`${classes.img} ${classes.tooltip}`}>
+              <img onClick={handleGoToSource} src="/output.png" alt="output" />
+              <span className={`${classes.tooltiptext} ${classes.tooltip_bottom}`}>해당 문제 페이지로 이동</span>  
+            </div>
+            <span className={classes.separator}>|</span>
             <span onClick={handleMoveToModifyPage} className={classes.link}>
               수정
             </span>
