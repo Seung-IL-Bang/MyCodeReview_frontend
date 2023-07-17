04520835af75781a9fbe537deeaf708591fa1587
Seungil <seungilbang@khu.ac.kr>
2023-07-17 15:24:02 +0900

    [UPD] SubReview.js: reducer 초기 상태에 따른 get 요청 여부 분기

--- src/component/subreview/SubReview.js
+++ src/component/subreview/SubReview.js
@@ -4,43 +4,54 @@ import { useParams } from "react-router";
 import Header from "../../layout/Header";
 import MarkdownViewer from "../review/MarkdownViewer";
 import { Spinner } from "react-bootstrap";
-
+import { useSelector } from "react-redux";
 
 export default function SubReview(props) {
+  const { id } = useParams(); // subReveiw's id
 
-    const { id } = useParams();
-    const [data, setData] = useState();
-    const [isLoading, setIsLoading] = useState(true)
+  const [data, setData] = useState();
+  const [boardId, setBoardId] = useState();
+  const [content, setContent] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        getSubReviewById(id)
-            .then(res => {
-                console.log(res)
-                setData(res)
-                setIsLoading(false)
-            })
-    }, [])
+  const mainReviewData = useSelector((state) => state.subReview.data);
+  const subContent = useSelector((state) => state.subReview.content);
 
-    const getSubReviewById = async (id) => {
-        const accessToken = localStorage.getItem('accessToken')
-
-        const response = await axios({
-            method: 'get',
-            url: `http://localhost:8080/auth/board/review/${id}`,
-            headers: {
-                'Authorization': `Bearer ${accessToken}`
-            }
-        })
-
-        return response.data
+  useEffect(() => {
+    if (!mainReviewData || !subContent) {
+      getSubReviewById(id).then((res) => {
+        console.log(res);
+        setData(res);
+        setBoardId(res.boardId);
+        setContent(res.content);
+        setIsLoading(false);
+      });
+    } else {
+      setData(mainReviewData);
+      setContent(subContent);
+      setBoardId(mainReviewData.id);
+      setIsLoading(false);
     }
+  }, []);
 
+  const getSubReviewById = async (id) => {
+    const accessToken = localStorage.getItem("accessToken");
 
+    const response = await axios({
+      method: "get",
+      url: `http://localhost:8080/auth/board/review/${id}`,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
 
-    return (
-        <>
-          <Header />
-          {!isLoading ? <MarkdownViewer data={data} id={data.boardId} /> : <Spinner animation="border" />}
-        </>
-    );
+    return response.data;
+  };
+
+  return (
+    <>
+      <Header />
+      {!isLoading ? <MarkdownViewer data={data} content={content} id={boardId} /> : <Spinner animation="border" />}
+    </>
+  );
 }
