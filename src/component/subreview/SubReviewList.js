3ace2aae7695112895a2426486c3837cc0c620f7
Seungil <seungilbang@khu.ac.kr>
2023-07-17 15:25:17 +0900

    [UPD] SubReviewList.js: 서브 리뷰 목록 조회 API 완성

--- src/component/subreview/SubReviewList.js
+++ src/component/subreview/SubReviewList.js
@@ -1,44 +1,49 @@
 import axios from "axios";
 import React from "react";
 import { Button } from "react-bootstrap";
-import classes from './SubReviewList.module.css'
+import classes from "./SubReviewList.module.css";
 import { useNavigate } from "react-router";
+import { useDispatch } from "react-redux";
+import { setData, setSubContent } from "../../reducer/subReviewReducer";
+import { setContent } from "../../reducer/metaDataReducer";
 
 export default function SubReviewList(props) {
-
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleGetMainBoard = () => {
-    navigate(`/review/${props.boardId}`)
-  }
+    window.location = `/review/${props.boardId}`
+  };
 
-  const handleGetSubReview = (subReviewId) => {
-    window.location = `/review/sub/${subReviewId}`
-  }
+  const handleGetSubReview = (subReviewId, subReviewContent) => {
+    dispatch(setData(props.data));
+    dispatch(setSubContent(subReviewContent));
+    navigate(`/review/sub/${subReviewId}`);
+  };
 
-  const handleAddReview = () => {
+  const handleAddReview = () => {};
 
-  }
-
-  const postSubReview = async () => {
-    
-    
-  }
-
-  const subReviews = props.subreviews.map((subReview, index) => (
+  const subReviews = props.data.reviewList.map((subReview, index) => (
     <div className={classes.subtitle} key={`subreview${subReview.id}`}>
       <span>{`${index + 1}. `}</span>
-      <span onClick={() => handleGetSubReview(subReview.id)}>{subReview.subTitle}</span>  
+      <span onClick={() => handleGetSubReview(subReview.id, subReview.content)}>
+        {subReview.subTitle}
+      </span>
     </div>
-  ))
+  ));
 
   return (
+    <div>
       <div>
-        <div>
-          <Button variant="primary" onClick={handleAddReview}>Add Review</Button> 
-          <div onClick={handleGetMainBoard} className={classes.subtitle}>{`0. ${props.boardTitle}`}</div>
-          {subReviews}
-        </div>
+        <Button variant="primary" onClick={handleAddReview}>
+          Add Review
+        </Button>
+        <div
+          onClick={handleGetMainBoard}
+          className={classes.subtitle}
+        >{`0. ${props.data.title}`}</div>
+        {subReviews}
       </div>
-  );  
+    </div>
+  );
 }
