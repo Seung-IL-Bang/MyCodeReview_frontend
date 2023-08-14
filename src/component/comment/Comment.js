581ca004624ed781bcdabd1d5a01f06fc208cf19
Seungil Linux <seungilbang@khu.ac.kr>
2023-08-14 17:05:52 +0900

    [UPD] Comment.js: 댓글 수정 추가

--- src/component/comment/Comment.js
+++ src/component/comment/Comment.js
@@ -7,7 +7,7 @@ export default function Comment(props) {
   return (
     <div>
       <CommentWrite boardId={props.boardId} onAddComment={props.onAddComment}/>
-      <CommentList comments={props.comments} onRemoveComment={props.onRemoveComment}/>
+      <CommentList comments={props.comments} boardId={props.boardId} onRemoveComment={props.onRemoveComment} onUpdateComment={props.onUpdateComment}/>
     </div>
   );
 }
